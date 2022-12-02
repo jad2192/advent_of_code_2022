89147d1ff1d8ae1a9b8166da89f1ebfff2262b3f
James Diotte <jamesdiotte@gmail.com>
2022-12-02 12:05:30 -0500

    use dict to get rid of if/elses

--- solutions/day2.py
+++ solutions/day2.py
@@ -53,7 +53,7 @@ def load_strategy_guide() -> List[str]:
 
 
 PLAY_D = dict(X=1, Y=2, Z=3, A=1, B=2, C=3)
-# Encode three winning situtions for tuple: (my_play, elfs_play)
+# Encode three winning situtions as tuples: (my_play, elfs_play)
 WIN_D: Set[Tuple[int, int]] = {(1, 3), (2, 1), (3, 2)}
 
 
@@ -70,21 +70,18 @@ def get_my_total() -> int:
 # Part 2
 WIN_D_PT2: Dict[str, str] = {"A": "B", "B": "C", "C": "A"}
 LOSE_D_PT2: Dict[str, str] = {val: key for key, val in WIN_D_PT2.items()}
+ACTION_D: Dict[str, Dict[str, str]] = {"X": LOSE_D_PT2, "Z": WIN_D_PT2, "Y": {x: x for x in "ABC"}}
 
 
 def alter_round(match_round: str) -> str:
-    """X - lose, Y - draw, Z - win"""
     elf, me = match_round[0], match_round[-1]
-    if me == "X":
-        return f"{elf} {LOSE_D_PT2[elf]}"
-    elif me == "Y":
-        return f"{elf} {elf}"
-    else:
-        return f"{elf} {WIN_D_PT2[elf]}"
+    return f"{elf} {ACTION_D[me][elf]}"
+
 
 def get_my_total_pt2() -> int:
     altered_rounds = [alter_round(round) for round in load_strategy_guide()]
     return sum([round_score(match) for match in altered_rounds])
+
 
 print("Part 1: ", get_my_total())
 print("Part 2: ", get_my_total_pt2())
