2eab74cbf2af9d31d29a72c0abcb403fe0217db5
James Diotte <jamesdiotte@gmail.com>
2022-12-06 07:20:58 -0500

    Day 6 Pt 2

--- solutions/day6.py
+++ solutions/day6.py
@@ -2,12 +2,15 @@ def load_data() -> str:
     return open("inputs/day6.txt").read().strip()
 
 
-def part_1() -> int:
+def signal_decoder(packet_length: int) -> int:
     """I know this is less efficient than using a stack, but I am a sucker for
     that sweet python list comprehension."""
     signal_string = load_data()
-    num_packet_chars = [len(set(signal_string[k - 4 : k])) for k in range(4, len(signal_string))]
-    return [k for k, length in enumerate(num_packet_chars) if length == 4][0] + 4
+    num_packet_chars = [
+        len(set(signal_string[k - packet_length : k])) for k in range(packet_length, len(signal_string))
+    ]
+    return [k for k, length in enumerate(num_packet_chars) if length == packet_length][0] + packet_length
 
 
-print("Part 1: ", part_1())
+print("Part 1: ", signal_decoder(4))
+print("Part 2: ", signal_decoder(14))
